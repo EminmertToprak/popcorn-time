259af0f9987bd355bfb2a63f81d6c7c92d53ef19
Mert Toprak <mert.tprk@gmail.com>
2023-11-08 11:49:24 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import './App.css'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
